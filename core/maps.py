a59d4bb9bb1647f805193a0240c9afaa2f76ece9
Tácio Tavares <taciogt@users.noreply.github.com>
2017-09-20 10:05:53 -0300

    mocks pdvs json file

--- core/maps.py
+++ core/maps.py
@@ -1,11 +1,14 @@
 import json
 
 
+MAPS_FILE = "./core/pdvs.json"
+
+
 def hello():
     return "Hello World"
 
 
 def read_data():
-    with open("./core/pdvs.json") as f:
+    with open(MAPS_FILE) as f:
         pdvs = json.load(f)
     return pdvs
--- core/test_maps.py
+++ core/test_maps.py
@@ -1,14 +1,22 @@
-import unittest
+import pytest
+import pprint
 
+from core import maps
 from core.maps import hello, read_data
 
 
-class TestHello(unittest.TestCase):
+@pytest.fixture(autouse=True)
+def set_up_tests():
+    print('set up class')
+    maps.MAPS_FILE = "./core/pdvs_test.json"
 
-    def test_dumb(self):
-        self.assertEqual(hello(), 'Hello World')
+
+def test_dumb():
+    assert hello() == 'Hello World'
 
 
-    def test_read_json(self):
-        pdvs = read_data()['pdvs']
-        assert len(pdvs) == 51
+def test_read_json():
+    pdvs = read_data()['pdvs']
+    assert len(pdvs) == 51
+    pprint.pprint(pdvs[0], indent=2)
+
